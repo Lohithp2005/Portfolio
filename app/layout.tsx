079cbebddbbba00ec974551dbaa3dd41f81d2fe1
Lohithp2005 <lohithcseng@gmail.com>
2026-09-21 18:41:14 +0530

    Removed missing component from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MobileMenu from "./components/mobile-menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
